5a6b29026fe3efa66f67599d648a93fdaf792364
root <root@ip-172-30-0-189>
2016-05-09 01:42:22 +0000

    implementation

--- lib/Performance/Probability.pm
+++ lib/Performance/Probability.pm
@@ -5,6 +5,9 @@ use strict;
 use warnings;
 use Moo;
 
+use Math::Cephes::Matrix;
+use Math::SymbolicX::Statistics::Distributions;
+
 our $VERSION = '0.01';
 
 =head1 NAME
@@ -44,10 +47,25 @@ has _l_k => (
     is       => 'rw',
 );
 
+sub _build__l_k {
+	my $buy_matrix = Math::Cephes::Matrix->($self->bought_price);
+        my $zero       = Math::Cepher::Matrix->($self->bought_price);
+        $zero->clr(0);
+  
+}
+
 has _p_k => (
     is       => 'rw',
 );
 
+sub _build__p_k {
+	my $buy_matrix = Math::Cephes::Matrix->new($self->bought_price);
+	my $payout_matrix = Math::Cephes::Matrix->new($self->payout);
+
+        my $p_k = $buy_matrix->div($payout_matrix);
+
+        return $p_k;
+}
 
 sub _mean_sigma_x {
 
