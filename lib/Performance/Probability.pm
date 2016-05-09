e68777824c1732b21b8a8762d897e9a2e4c642cc
root <root@ip-172-30-0-189>
2016-05-09 05:22:06 +0000

    editing

--- lib/Performance/Probability.pm
+++ lib/Performance/Probability.pm
@@ -43,15 +43,27 @@ has _w_k => (
     is       => 'rw',
 );
 
+sub _build__w_k {
+	my $payout_matrix = Math::Cephes::Matrix->new($self->payout);
+	my $buy_matrix    = Math::Cephes::Matrix->new($self->buy_matrix);
+
+        my $w_k = $payout->sub($buy_matrix);
+
+        return $w_k;
+}
+
 has _l_k => (
     is       => 'rw',
 );
 
 sub _build__l_k {
-	my $buy_matrix = Math::Cephes::Matrix->($self->bought_price);
-        my $zero       = Math::Cepher::Matrix->($self->bought_price);
+	my $buy_matrix = Math::Cephes::Matrix->new($self->bought_price);
+        my $zero       = Math::Cepher::Matrix->new($self->bought_price);
         $zero->clr(0);
-  
+       
+        my $l_k = $zero->sub($buy_matrix);
+	
+	return $l_k;  
 }
 
 has _p_k => (
@@ -67,12 +79,31 @@ sub _build__p_k {
         return $p_k;
 }
 
+# sum( wk*pk + lk * (1-pk) )
 sub _mean_sigma_x {
+	
+	my $wk_pk = $self->_w_k->mul($self->_p_k);
 
+        my $one = ;
+
+        my $one_minus_pk = $one->sub($self->_p_k);
+
+        my $lk_pk = $self->_l_k->mul($self->one_minus_pk);	
+
+	my $tmp = $wk_pk->add($lk_pk);
+
+	#sum all elements in $tmp.
+	
 }
 
 sub _variance_sigma_x {
+   
+       my $wk_t = $self->_w_k->transp();
+       my $wk_square = $wk_t->mul($self->_w_k);
+	
+       my $wk2_pk = $wk_square->mul($self->_p_k);
 
+       
 }
 
 sub _covariance {
