feac296c0b09cfbc3d576d0d8b4ac0991cc0d46e
Shahrizal Shaari <shahrizal@binary.com>
2016-06-06 05:46:34 +0000

    debug

--- lib/Performance/Probability.pm
+++ lib/Performance/Probability.pm
@@ -186,7 +186,7 @@ sub _covariance {
 
 =head2 get_performance_probability
 
-Calculate performance probability ( modified sharpe ratio 
+Calculate performance probability ( modified sharpe ratio ) 
 
 =cut
 
